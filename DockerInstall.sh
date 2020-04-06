b4faa3db63662cd50cd5e255355fc20ac93528e8
AliS2018 <41644264+AliS2018@users.noreply.github.com>
2020-04-06 20:14:57 +0200

    Add files via upload

--- DockerInstall.sh
+++ DockerInstall.sh
@@ -9,7 +9,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 apt update
-apt install docker-ce -yw
+apt install docker-ce -y
 ##Install docker-compose
 curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
